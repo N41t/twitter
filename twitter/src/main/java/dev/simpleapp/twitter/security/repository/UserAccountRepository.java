1e2efad4f0d75e9509688a544349280110491a2f
Mikhail Belozerov <belozerov.mg@gmail.com>
2023-10-20 12:22:23 +0700

    Added Basic Auth

--- twitter/src/main/java/dev/simpleapp/twitter/security/repository/UserAccountRepository.java
+++ twitter/src/main/java/dev/simpleapp/twitter/security/repository/UserAccountRepository.java
@@ -3,7 +3,14 @@ package dev.simpleapp.twitter.security.repository;
 import dev.simpleapp.twitter.security.model.UserAccount;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+
+import java.util.Optional;
+
 public interface UserAccountRepository extends JpaRepository<UserAccount, Long> {
 
     boolean existsByUsername(String username);
+
+
+    Optional<UserAccount> findByUsername(String username);
+
 }
